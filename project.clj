a9689abb7a9ec2bd7ee31de278bdc22bf24fd44d
diogo <diogosouzac@gmail.com>
2024-11-09 13:54:29 -0300

    a few changes

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/core.cache "1.1.234"]
                  [cljfx "1.9.3"]
-                 [clj-pdf "2.6.9"]]
+                 [clj-pdf "2.6.9"]
+                 [seancorfield/depstar "0.5.2"]]
   :repl-options {:init-ns editor.core
                  :init    (require '[clojure.repl :refer :all])}
   :main editor.core
